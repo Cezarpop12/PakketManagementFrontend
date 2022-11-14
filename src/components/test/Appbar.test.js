736eadc01e4f81dc81a189daa241c65738e9f402
CezarPop12 <mr.chaser31@yahoo.com>
2022-11-14 19:27:59 +0100

    Nieuwe test

--- src/components/test/Appbar.test.js
+++ src/components/test/Appbar.test.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Appbar from '../Appbar';
 import '@testing-library/jest-dom/extend-expect'
+import { BrowserRouter } from 'react-router-dom';
 
 test('should render Appbar component', () => {
 
-    render(<Appbar/>);
+    render(<BrowserRouter><Appbar/></BrowserRouter>);
 
     const AppbarElement = screen.getByTestId('the_man');
 

cccb294fa8103c3158133fb2165c96978d726516
CezarPop12 <mr.chaser31@yahoo.com>
2022-11-15 13:27:29 +0100

    new fetch getAll

--- src/pagina/HomePagina.js
+++ src/pagina/HomePagina.js
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { Paper, Button } from '@mui/material';
+import { Container, Paper, Button } from '@mui/material';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import Appbar from '../components/Appbar';
 import App from '../App';
 
 export default function HomePagina() {
   const paperStyle = { padding: '50px 20px', width: 600, margin: "20px auto" }
-  // const [code, setCode] = React.useState('')
+  const [code, setCode] = React.useState('')
   const [pakketjes, setPakketjes] = React.useState([])
 
   //useffect = voor bij pagina starten om info te loaden, in dit geval alle pakketjes tonen bij opstart
@@ -21,25 +21,24 @@ export default function HomePagina() {
       )
   }, [])
 
-  const element = document.querySelector('#put-request .date-updated');
-  const requestOptions = {
-    method: 'PUT',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ title: 'Fetch PUT Request' })
-  };
-  fetch('http://localhost:8080/pakketje/updateOnderweg', requestOptions)
-    .then(response => response.json())
-    .then(data => element.innerHTML = data.updatedAt);
+  // const element = document.querySelector('#put-request .date-updated');
+  // const requestOptions = {
+  //   method: 'PUT',
+  //   headers: { 'Content-Type': 'application/json' },
+  //   body: JSON.stringify({ title: 'Fetch PUT Request' })
+  // };
+  // fetch('http://localhost:8080/pakketje/updateOnderweg', requestOptions)
+  //   .then(response => response.json())
+  //   .then(data => element.innerHTML = data.updatedAt);
 
   return (
     <>
     <Appbar />
-    <container>
       <h1>Pakketjes</h1>
       <Paper elevation={3} style={paperStyle}>
         {pakketjes.map(pakketje => (
           <Paper elevation={6} style={{ margin: "10px", padding: "15px", textAlign: "left" }} key={pakketje.id}>
-            <b>Pakketje&emsp;&emsp;&emsp;&emsp;Status: </b>{pakketje.status}
+            <b>Pakketje&emsp;&emsp;&emsp;&emsp;</b>Status: {pakketje.status}
             <br />
             <br />
             ID: {pakketje.id}&emsp;&emsp;&emsp;
@@ -57,7 +56,6 @@ export default function HomePagina() {
         ))
         }
       </Paper>
-    </container>
     </>
   );
 }
